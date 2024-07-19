0064ce6c0ef9c7ed330532f59c53e3b4216e8391
Ikhwan kurniawan <128669107+rhenaald@users.noreply.github.com>
2024-07-19 19:00:55 +0700

    create header component

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,21 @@
+<header class="bg-white shadow">
+    <div class="mx-auto max-w-4xl px-4 sm:px-6 lg:px-8">
+      <div class="flex h-12 items-center justify-between">
+        <div class="flex items-center justify-center w-full">
+          <div class="flex-shrink-0">
+          <div class="hidden md:block">
+            <div class="flex items-baseline space-x-4">
+              <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+              <a href="/" class="rounded-md px-3 py-2 text-sm font-medium text-gray-900 hover:bg-gray-400 hover:text-white" aria-current="page">Home</a>
+              <a href="/about" class="rounded-md px-3 py-2 text-sm font-medium text-gray-900 hover:bg-gray-400 hover:text-white">About</a>
+              <a href="/blog" class="rounded-md px-3 py-2 text-sm font-medium text-black-900 hover:bg-gray-400 hover:text-white">Blog</a>
+              <a href="/contact" class="rounded-md px-3 py-2 text-sm font-medium text-black-900 hover:bg-gray-400 hover:text-white">Contact</a>
+            </div>
+          </div>
+          </div>
+        </div>
+    </div>
+</div>
+       
+    
+</header>
