c89b4e2a0055a79a64a9b3dcf40aa856674f68ad
Ikhwan kurniawan <128669107+rhenaald@users.noreply.github.com>
2024-07-19 16:09:07 +0700

    create contact page

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>contact</title>
+</head>
+<body>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/blog">Blog</a>
+    <a href="/Contact">Contact</a>
+    <h1>Halaman contact</h1>
+
+    <ul>
+        <li><a href="">Instagram</a></li>
+        <li><a href="">Youtube</a></li>
+        <li><a href="">Facebook</a></li>
+    </ul>
+</body>
+</html>
