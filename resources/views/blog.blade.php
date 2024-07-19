c8abeac790c6b0d276bcd4f20ab7478fc09e0fca
Ikhwan kurniawan <128669107+rhenaald@users.noreply.github.com>
2024-07-19 19:00:37 +0700

    create blog page with layout

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,25 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Blog</title>
-</head>
-<body>
-    <a href="/">Home</a>
-    <a href="/about">About</a>
-    <a href="/blog">Blog</a>
-    <a href="/Contact">Contact</a>
-    <h1>Halaman Blog</h1>
-
-    <div>
-        <h3>artikel 1</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias pariatur ullam mollitia magnam sit eveniet, quia quae aspernatur temporibus explicabo! Nobis, distinctio illum tenetur optio numquam quos voluptates doloribus earum.</p>
-    </div>
-
-    <div>
-        <h3>artikel 2</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias pariatur ullam mollitia magnam sit eveniet, quia quae aspernatur temporibus explicabo! Nobis, distinctio illum tenetur optio numquam quos voluptates doloribus earum.</p>
-    </div>
-</body>
-</html>
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot>
+    <h3>Blog page</h3>
+</x-layout>
