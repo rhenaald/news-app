2adef35c67cb547856f367ca3330ebe6bc529596
Ikhwan kurniawan <128669107+rhenaald@users.noreply.github.com>
2024-07-19 16:08:20 +0700

    bikin route untuk halaman lain

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,19 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('About');
+});
+
+Route::get('/blog', function () {
+    return view('blog');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
 });
 
 Route::middleware([
