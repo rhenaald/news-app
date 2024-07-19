ff5baf1cdf9a6d8ec6b92c864965627f85e25bf0
Ikhwan kurniawan <128669107+rhenaald@users.noreply.github.com>
2024-07-19 19:00:27 +0700

    create about page with layout

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,15 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>About</title>
-</head>
-<body>
-    <a href="/">Home</a>
-    <a href="/about">About</a>
-    <a href="/blog">Blog</a>
-    <a href="/Contact">Contact</a>
-    <h1>Halaman About</h1>
-</body>
-</html>
+
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot>
+    <h3>About page</h3>
+</x-layout>
