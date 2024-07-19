a49ef911378a9621515db2a0579cf2aeed8268bf
Ikhwan kurniawan <128669107+rhenaald@users.noreply.github.com>
2024-07-19 19:01:50 +0700

    create contact page with layout

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,21 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>contact</title>
-</head>
-<body>
-    <a href="/">Home</a>
-    <a href="/about">About</a>
-    <a href="/blog">Blog</a>
-    <a href="/Contact">Contact</a>
-    <h1>Halaman contact</h1>
 
-    <ul>
-        <li><a href="">Instagram</a></li>
-        <li><a href="">Youtube</a></li>
-        <li><a href="">Facebook</a></li>
-    </ul>
-</body>
-</html>
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot>
+    <h3>Home page</h3>
+</x-layout>
