ccc72a6b6ee18f9a99885e99e52bd765d132c7b5
Ikhwan kurniawan <128669107+rhenaald@users.noreply.github.com>
2024-07-20 21:19:00 +0700

    membuat post di home menggunakan array

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,17 @@
-
 <x-layout>
     <x-slot:title>{{ $title }}</x-slot>
-    <h3>Home page</h3>
+    @foreach ($posts as $post)
+        <article class="py-8 max-w-screen-md border-b border-gray-300">
+            <a href="/posts/{{ $post['slug'] }}" class="hover:underline">
+                <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">
+                    {{ $post['title'] }}
+                </h2>
+            </a>
+            <div class="font-base text-gray-500">
+                <a href="#">{{ $post['author'] }}</a> | 20 Juli 2024
+            </div>
+            <p class="my-4 font-light">{{ Str::limit($post['body'], 100) }}</p>
+            <a href="/posts/{{ $post['slug'] }}" class="font-medium text-blue-500 hover:underline">Read More &raquo;</a>
+        </article>
+    @endforeach
 </x-layout>
