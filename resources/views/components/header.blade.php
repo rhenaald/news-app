086470d6c89548350c0b176f8a854c4a550d6eed
Ikhwan kurniawan <128669107+rhenaald@users.noreply.github.com>
2024-07-20 21:18:26 +0700

    hapus komen

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,15 +1,11 @@
 <header class="bg-white shadow">
   <div class="mx-auto max-w-4xl px-4 sm:px-6 lg:px-8">
     <div class="flex h-12 items-center justify-between">
-      <!-- Logo atau Elemen Kiri -->
       <div class="flex-shrink-0">
         <!-- Tempat logo atau elemen lain -->
       </div>
-
-      <!-- Menu Desktop -->
       <div class="hidden md:flex md:items-center md:justify-center w-full">
         <div class="flex items-baseline space-x-4">
-          <!-- Menu Items -->
           <x-nav-link-header-news type="default" href="/politik" :active="request()->is('politik')">Politik</x-nav-link-header-news>
           <x-nav-link-header-news type="default" href="/hukum" :active="request()->is('hukum')">Hukum</x-nav-link-header-news>
           <x-nav-link-header-news type="default" href="/pendidikan" :active="request()->is('pendidikan')">Pendidikan</x-nav-link-header-news>
@@ -17,13 +13,9 @@
           <x-nav-link-header-news type="default" href="/ekonomi" :active="request()->is('ekonomi')">Ekonomi</x-nav-link-header-news>
         </div>
       </div>
-
-      <!-- Menu Mobile -->
       <div class="md:hidden w-full">
         <div class="px-4 pt-2">
-          <!-- Wrapper for horizontal scrolling -->
           <div class="flex space-x-4">
-            <!-- Mobile Menu Items -->
             <x-nav-link-header-news type="default" href="/politik" :active="request()->is('politik')" mobile="true">Politik</x-nav-link-header-news>
             <x-nav-link-header-news type="default" href="/hukum" :active="request()->is('hukum')" mobile="true">Hukum</x-nav-link-header-news>
             <x-nav-link-header-news type="default" href="/pendidikan" :active="request()->is('pendidikan')" mobile="true">Pendidikan</x-nav-link-header-news>
