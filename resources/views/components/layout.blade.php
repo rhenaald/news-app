7e235637b178fe12d7a54709d104c716e7416e2b
Ikhwan kurniawan <128669107+rhenaald@users.noreply.github.com>
2024-07-21 22:30:37 +0700

    menggabungkan header ke navbar

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -11,7 +11,6 @@
 <body class="h-full">
 <div class="min-h-full">
     <x-navbar></x-navbar>
-    <x-header></x-header>
   <main>
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
       {{ $slot }}
