5e1637f70761f90c32c98789ece16df8a914bae7
Ikhwan kurniawan <128669107+rhenaald@users.noreply.github.com>
2024-07-21 22:31:21 +0700

    membuat authornya menjadi hyperlink

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,6 @@
 <x-layout>
     <x-slot:title>{{ $title }}</x-slot>
+    <h2 class="mb-1 text-4xl tracking-tight font-bold text-sky-600">{{$sub_judul??""}}</h2>
     @foreach ($posts as $post)
         <article class="py-8 max-w-screen-md border-b border-gray-300">
             <a href="/posts/{{ $post['slug'] }}" class="hover:underline">
@@ -7,8 +8,8 @@
                     {{ $post['title'] }}
                 </h2>
             </a>
-            <div class="font-base text-gray-500">
-                <a href="#">{{ $post['author'] }}</a> | 20 Juli 2024
+            <div class="font-base text-gray-500 ">
+                <a href="/authors/{{ $post->author->id }}" class="hover:underline">{{ $post->author->name }}</a> | {{$post->created_at->diffForHumans()}}
             </div>
             <p class="my-4 font-light">{{ Str::limit($post['body'], 100) }}</p>
             <a href="/posts/{{ $post['slug'] }}" class="font-medium text-blue-500 hover:underline">Read More &raquo;</a>
