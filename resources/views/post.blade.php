ecda127e33f72d5d3fbc59c05aa2b0f15f8f9214
Ikhwan kurniawan <128669107+rhenaald@users.noreply.github.com>
2024-07-21 22:32:33 +0700

    mengedit tanggal sesuai waktu

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,13 +2,14 @@
     <x-slot:title>{{ $title }}</x-slot>
     <article class="py-8 max-w-screen-md border-b border-gray-300">
         <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">
-            {{ $post['title'] }}
+            {{ $post->title }} <!-- Akses properti menggunakan -> -->
         </h2>
 
         <div class="font-base text-gray-500">
-            <a href="#">{{ $post['author'] }}</a> | 20 Juli 2024
+            <a href="/authors/{{ $post->author->name }}">{{ $post->author->name }}</a> <!-- Akses nama penulis -->
+            | {{ $post->created_at->diffForHumans() }}
         </div>
-        <p class="my-4 font-light">{{ $post['body'] }}</p>
-        <a href="/" class="font-medium text-blue-500 hover:underline">&laquo; Back to home</a>
+        <p class="my-4 font-light">{{ $post->body }}</p>
+        <a href="/" class="font-medium text-blue-500 hover:underline">&laquo; Kembali ke beranda</a>
     </article>
 </x-layout>
