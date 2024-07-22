80b36801f04764263648949e0d1c4c78faaf1009
Ikhwan kurniawan <128669107+rhenaald@users.noreply.github.com>
2024-07-22 11:23:03 +0700

    relasi post ke kategori

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,16 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo; // Make sure this is imported
+use Illuminate\Database\Eloquent\Relations\BelongsTo; 
 
 class Post extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title', 'author', 'slug', 'body']; // Changed 'author' to 'author_id'
+    protected $fillable = ['title', 'author', 'slug', 'body']; 
 
     public function author(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
     }
 }
