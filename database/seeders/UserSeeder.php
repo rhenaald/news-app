6b64c7944147f6991eac9f10d09420080eb44857
Ikhwan kurniawan <128669107+rhenaald@users.noreply.github.com>
2024-07-22 15:44:13 +0700

    Create UserSeeder.php

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Seeders;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // User::Create([
+        //     'name' => 'ikhwan Kurniawan Julianto',
+        //     'username' => "ikhwan-kurniawan",
+        //     'email' => 'ikhwan@example.com',
+        //     'email_verified_at' => now(),
+        //     'password' => static::$password ??= Hash::make('password'),
+        //     'two_factor_secret' => null,
+        //     'two_factor_recovery_codes' => null,
+        //     'remember_token' => Str::random(10),
+        //     'profile_photo_path' => null,
+        //     'current_team_id' => null,
+        // ]);
+
+        User::factory()->create([
+            'name' => 'ikhwan Kurniawan Julianto',
+            'username' => "ikhwan-kurniawan",
+            'email' => 'ikhwan@example.com',
+        ]);
+        
+        User::factory()->create([
+            'name' => 'Delvina Salma Hidayah',
+            'username' => "delvina-salma",
+            'email' => 'delvina@example.com',
+        ]);
+
+        User::factory(1)->create();
+    }
+}
