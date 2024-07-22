37d3b57ad204c284c660ca02c680c4445419dc50
Ikhwan kurniawan <128669107+rhenaald@users.noreply.github.com>
2024-07-22 11:25:02 +0700

    menambahkan kategori dan edit css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,8 +8,12 @@
                     {{ $post['title'] }}
                 </h2>
             </a>
-            <div class="font-base text-gray-500 ">
-                <a href="/authors/{{ $post->author->id }}" class="hover:underline">{{ $post->author->name }}</a> | {{$post->created_at->diffForHumans()}}
+            <div>
+                By 
+                <a href="/categories/{{ $post->category->slug }}" class="font-base text-gray-500 hover:underline">{{ $post->author->name }}</a> 
+                in
+                <a href="/categories/{{ $post->category->slug }}" class="font-base text-gray-500 hover:underline">{{ $post->category->name }}</a>
+                | {{$post->created_at->diffForHumans()}}
             </div>
             <p class="my-4 font-light">{{ Str::limit($post['body'], 100) }}</p>
             <a href="/posts/{{ $post['slug'] }}" class="font-medium text-blue-500 hover:underline">Read More &raquo;</a>
