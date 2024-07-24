87a1cb85a4661ec8efeb2eb1792c9a6d2a22d058
Ikhwan kurniawan <128669107+rhenaald@users.noreply.github.com>
2024-07-24 10:41:27 +0700

    nothing special just friend

--- app/Models/User.php
+++ app/Models/User.php
@@ -63,7 +63,7 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
-
+    
     public function posts(): HasMany
     {
         return $this->hasMany(Post::class, 'author_id');
