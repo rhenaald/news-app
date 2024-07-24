02cca9f6b938ca4992788e4b0ae32950b7c5c125
Ikhwan kurniawan <128669107+rhenaald@users.noreply.github.com>
2024-07-24 10:41:09 +0700

    create soft dellete

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,11 +5,20 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
+    use SoftDeletes;
     use HasFactory;
 
+    protected $fillable = [
+        'name', 
+        'slug', 
+        'color', 
+        ]; 
+
+    protected $hidden =[];
     
     public function posts(): HasMany
     {
