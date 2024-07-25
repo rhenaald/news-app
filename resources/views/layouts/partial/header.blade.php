14e4999fd2e2ec9dfa661bbbea3bcf23cc2797d6
Ikhwan kurniawan <128669107+rhenaald@users.noreply.github.com>
2024-07-25 21:09:16 +0700

    Update header.blade.php

--- resources/views/layouts/partial/header.blade.php
+++ resources/views/layouts/partial/header.blade.php
@@ -14,19 +14,15 @@
         <!-- header -->
         <div class="flex items-center justify-between p-4 bg-white-100 dark:bg-gray-900">
             <div class="flex items-center space-x-2"> 
-            <svg class="w-8 h-8" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-               <defs>
-                  <linearGradient id="blueGradient" x1="0%" y1="0%" x2="100%" y2="100%">
-                     <stop offset="0%" style="stop-color:#2563eb; stop-opacity:1" />
-                     <stop offset="100%" style="stop-color:#1d4ed8; stop-opacity:1" />
-                  </linearGradient>
-               </defs>
-               <path fill="none" stroke="url(#blueGradient)" stroke-linecap="round" stroke-width="2" d="M8.737 8.737a21.49 21.49 0 0 1 3.308-2.724m0 0c3.063-2.026 5.99-2.641 7.331-1.3 1.827 1.828.026 6.591-4.023 10.64-4.049 4.049-8.812 5.85-10.64 4.023-1.33-1.33-.736-4.218 1.249-7.253m6.083-6.11c-3.063-2.026-5.99-2.641-7.331-1.3-1.827 1.828-.026 6.591 4.023 10.64m3.308-9.34a21.497 21.497 0 0 1 3.308 2.724m2.775 3.386c1.985 3.035 2.579 5.923 1.248 7.253-1.336 1.337-4.245.732-7.295-1.275M14 12a2 2 0 1 1-4 0 2 2 0 0 1 4 0Z"/>
-            </svg>
-            <h1 class="text-4xl font-bold text-white dark:text-white">Neta</h1>
-            <p></p>
+               <svg class="w-10 h-10 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                  <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M8.737 8.737a21.49 21.49 0 0 1 3.308-2.724m0 0c3.063-2.026 5.99-2.641 7.331-1.3 1.827 1.828.026 6.591-4.023 10.64-4.049 4.049-8.812 5.85-10.64 4.023-1.33-1.33-.736-4.218 1.249-7.253m6.083-6.11c-3.063-2.026-5.99-2.641-7.331-1.3-1.827 1.828-.026 6.591 4.023 10.64m3.308-9.34a21.497 21.497 0 0 1 3.308 2.724m2.775 3.386c1.985 3.035 2.579 5.923 1.248 7.253-1.336 1.337-4.245.732-7.295-1.275M14 12a2 2 0 1 1-4 0 2 2 0 0 1 4 0Z"/>
+               </svg>
+               <div class="flex flex-col">
+                  <h1 class="text-4xl font-bold text-white dark:text-white">Neta</h1>
+                  <p class="text-sm text-white dark:text-gray-400">News Talenthub</p>
+               </div>
             </div>
-            <button data-drawer-target="default-sidebar" data-drawer-toggle="default-sidebar" aria-controls="default-sidebar" type="button" class="inline-flex items-center p-2 text-white-500 rounded-lg sm:hidden hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-300 dark:text-white-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+            <button data-drawer-target="default-sidebar" data-drawer-toggle="default-sidebar" aria-controls="default-sidebar" type="button" class="inline-flex items-center p-2 text-white-500 rounded-lg sm:hidden hover:bg-white focus:outline-none focus:ring-2 focus:ring-gray-300 dark:text-white-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
                <span class="sr-only">Close sidebar</span>
                <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                   <path clip-rule="evenodd" fill-rule="evenodd" d="M6.293 4.293a1 1 0 0 1 1.414 0L10 6.586l2.293-2.293a1 1 0 0 1 1.414 1.414L11.414 8l2.293 2.293a1 1 0 0 1-1.414 1.414L10 9.414l-2.293 2.293a1 1 0 0 1-1.414-1.414L8.586 8 6.293 5.707a1 1 0 0 1 0-1.414z"></path>
@@ -46,6 +42,7 @@
          </li>
 
          <!-- kategori -->
+         @role('admin')
          <li>
             <a href="{{route('categories.index')}}" class="flex items-center p-2 text-white rounded-lg dark:text-white hover:bg-white hover:drop-shadow-sm dark:hover:bg-gray-700 group hover:text-gray-900">
                <svg class="flex-shrink-0 w-5 h-5 text-white transition duration-75 dark:text-white-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 18">
@@ -64,6 +61,7 @@
                   <span class="flex-1 ms-3 whitespace-nowrap hover:text-gray-900">User</span>
             </a>
          </li>
+         @endrole
 
          <!-- post -->
          <li>
@@ -79,7 +77,7 @@
          <p class="text-white-500 dark:text-white-400"></p>
          <!-- profile -->
          <li>
-            <a href="#" class="flex items-center p-2 text-white rounded-lg dark:text-white hover:bg-white hover:drop-shadow-sm dark:hover:bg-gray-700 group hover:text-gray-900">
+            <a href="/profile" class="flex items-center p-2 text-white rounded-lg dark:text-white hover:bg-white hover:drop-shadow-sm dark:hover:bg-gray-700 group hover:text-gray-900">
                <svg class="flex-shrink-0 w-5 h-5 text-white transition duration-75 dark:text-white-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                   <path d="m17.418 3.623-.018-.008a6.713 6.713 0 0 0-2.4-.569V2h1a1 1 0 1 0 0-2h-2a1 1 0 0 0-1 1v2H9.89A6.977 6.977 0 0 1 12 8v5h-2V8A5 5 0 1 0 0 8v6a1 1 0 0 0 1 1h8v4a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-4h6a1 1 0 0 0 1-1V8a5 5 0 0 0-2.582-4.377ZM6 12H4a1 1 0 0 1 0-2h2a1 1 0 0 1 0 2Z"/>
                </svg>
@@ -87,26 +85,18 @@
             </a>
          </li>
 
-         <!-- sign in -->
-         <li>
-            <a href="#" class="flex items-center p-2 text-white rounded-lg dark:text-white hover:bg-white hover:drop-shadow-sm dark:hover:bg-gray-700 group hover:text-gray-900">
-               <svg class="flex-shrink-0 w-5 h-5 text-white transition duration-75 dark:text-white-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 16">
-                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3"/>
-               </svg>
-               <span class="flex-1 ms-3 whitespace-nowrap">Sign In</span>
-            </a>
-         </li>
-
          <!-- sign up -->
          <li>
-            <a href="#" class="flex items-center p-2 text-white rounded-lg dark:text-white hover:bg-white hover:drop-shadow-sm dark:hover:bg-gray-700 group hover:text-gray-900">
-               <svg class="flex-shrink-0 w-5 h-5 text-white transition duration-75 dark:text-white-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                  <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.96 2.96 0 0 0 .13 5H5Z"/>
-                  <path d="M6.737 11.061a2.961 2.961 0 0 1 .81-1.515l6.117-6.116A4.839 4.839 0 0 1 16 2.141V2a1.97 1.97 0 0 0-1.933-2H7v5a2 2 0 0 1-2 2H0v11a1.969 1.969 0 0 0 1.933 2h12.134A1.97 1.97 0 0 0 16 18v-3.093l-1.546 1.546c-.413.413-.94.695-1.513.81l-3.4.679a2.947 2.947 0 0 1-1.85-.227 2.96 2.96 0 0 1-1.635-3.257l.681-3.397Z"/>
-                  <path d="M8.961 16a.93.93 0 0 0 .189-.019l3.4-.679a.961.961 0 0 0 .49-.263l6.118-6.117a2.884 2.884 0 0 0-4.079-4.078l-6.117 6.117a.96.96 0 0 0-.263.491l-.679 3.4A.961.961 0 0 0 8.961 16Zm7.477-9.8a.958.958 0 0 1 .68-.281.961.961 0 0 1 .682 1.644l-.315.315-1.36-1.36.313-.318Zm-5.911 5.911 4.236-4.236 1.359 1.359-4.236 4.237-1.7.339.341-1.699Z"/>
-               </svg>
-               <span class="flex-1 ms-3 whitespace-nowrap">Sign Up</span>
-            </a>
+            <form method="POST" action="{{ route('logout') }}" x-data>
+               <a href="{{ route('login') }}" @click.prevent="$root.submit();" class="flex items-center p-2 text-white rounded-lg dark:text-white hover:bg-white hover:drop-shadow-sm dark:hover:bg-gray-700 group hover:text-gray-900">
+                  <svg class="flex-shrink-0 w-5 h-5 text-white transition duration-75 dark:text-white-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                     <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.96 2.96 0 0 0 .13 5H5Z"/>
+                     <path d="M6.737 11.061a2.961 2.961 0 0 1 .81-1.515l6.117-6.116A4.839 4.839 0 0 1 16 2.141V2a1.97 1.97 0 0 0-1.933-2H7v5a2 2 0 0 1-2 2H0v11a1.969 1.969 0 0 0 1.933 2h12.134A1.97 1.97 0 0 0 16 18v-3.093l-1.546 1.546c-.413.413-.94.695-1.513.81l-3.4.679a2.947 2.947 0 0 1-1.85-.227 2.96 2.96 0 0 1-1.635-3.257l.681-3.397Z"/>
+                     <path d="M8.961 16a.93.93 0 0 0 .189-.019l3.4-.679a.961.961 0 0 0 .49-.263l6.118-6.117a2.884 2.884 0 0 0-4.079-4.078l-6.117 6.117a.96.96 0 0 0-.263.491l-.679 3.4A.961.961 0 0 0 8.961 16Zm7.477-9.8a.958.958 0 0 1 .68-.281.961.961 0 0 1 .682 1.644l-.315.315-1.36-1.36.313-.318Zm-5.911 5.911 4.236-4.236 1.359 1.359-4.236 4.237-1.7.339.341-1.699Z"/>
+                  </svg>
+                  <span class="flex-1 ms-3 whitespace-nowrap">Sign Up</span>
+               </a>
+            </form>
          </li>
       </ul>
    </div>
