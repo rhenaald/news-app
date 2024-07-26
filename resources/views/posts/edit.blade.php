606cfd96815d6826770b60c8fe55dedb932c3688
Ikhwan kurniawan <128669107+rhenaald@users.noreply.github.com>
2024-07-26 18:59:25 +0700

    Update edit.blade.php

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -21,18 +21,6 @@
         @enderror
     </div>
 
-    <div class="mb-5">
-        <label for="author" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Author</label>
-        <select id="author" name="author_id" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
-            <option value="" disabled>Pilih Author</option>
-            @foreach($authors as $author)
-                <option value="{{ $author->id }}" {{ $post->author_id == $author->id ? 'selected' : '' }}>{{ $author->name }}</option>
-            @endforeach
-        </select>
-        @error('author_id')
-            <div class="text-red-500 mt-2">{{ $message }}</div>
-        @enderror
-    </div>
 
     <div class="mb-5">
         <label for="category" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Kategori</label>
@@ -47,13 +35,6 @@
         @enderror
     </div>
 
-    <div class="mb-5">
-        <label for="foto" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Foto Postingan</label>
-        <input type="file" id="foto" name="foto" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-        @error('foto')
-            <div class="text-red-500 mt-2">{{ $message }}</div>
-        @enderror
-    </div>
 
     <a href="{{ route('categories.index') }}"  class="text-white bg-sky-600 hover:bg-sky-800 focus:ring-4 focus:outline-none focus:ring-sky-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-3 text-center dark:bg-sky-600 dark:hover:bg-sky-700 dark:focus:ring-sky-800">
         Kembali</a>
