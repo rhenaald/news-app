449a184035af9885ef59874b32c440870755bff4
Ikhwan kurniawan <128669107+rhenaald@users.noreply.github.com>
2024-07-26 18:56:35 +0700

    Update header.blade.php

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -6,21 +6,26 @@
       </div>
       <div class="hidden md:flex md:items-center md:justify-center w-full">
         <div class="flex items-baseline space-x-4">
-          <x-nav-link-header-news type="default" href="/politik" :active="request()->is('politik')">Politik</x-nav-link-header-news>
-          <x-nav-link-header-news type="default" href="/hiburan" :active="request()->is('hukum')">Hiburan</x-nav-link-header-news>
+        @foreach($categories as $category)
+          <x-nav-link-header-news type="default" href="/category/{{ $category->slug }}" :active="request()->is('categories/' . $category->slug)">{{ $category->name }}</x-nav-link-header-news>
+        @endforeach
+          <!-- <x-nav-link-header-news type="default" href="/hiburan" :active="request()->is('hukum')">Hiburan</x-nav-link-header-news>
           <x-nav-link-header-news type="default" href="/pendidikan" :active="request()->is('pendidikan')">Pendidikan</x-nav-link-header-news>
           <x-nav-link-header-news type="default" href="/olahraga" :active="request()->is('olahraga')">Olahraga</x-nav-link-header-news>
-          <x-nav-link-header-news type="default" href="/ekonomi" :active="request()->is('ekonomi')">Ekonomi</x-nav-link-header-news>
+          <x-nav-link-header-news type="default" href="/ekonomi" :active="request()->is('ekonomi')">Ekonomi</x-nav-link-header-news> -->
         </div>
       </div>
       <div class="md:hidden md:items-center md:justify-center w-full">
         <div class="px-4 pt-2">
           <div class="flex space-x-4">
-            <x-nav-link-header-news type="default" href="/politik" :active="request()->is('politik')" mobile="true">Politik</x-nav-link-header-news>
+          @foreach($categories as $category)
+          <x-nav-link-header-news type="default" href="/category/{{ $category->slug }}" :active="request()->is('categories/' . $category->slug)" mobile="true">{{ $category->name }}</x-nav-link-header-news>
+        @endforeach
+            <!-- <x-nav-link-header-news type="default" href="/politik" :active="request()->is('politik')" mobile="true">Politik</x-nav-link-header-news>
             <x-nav-link-header-news type="default" href="/hiburan" :active="request()->is('hukum')" mobile="true">Hiburan</x-nav-link-header-news>
             <x-nav-link-header-news type="default" href="/pendidikan" :active="request()->is('pendidikan')" mobile="true">Pendidikan</x-nav-link-header-news>
             <x-nav-link-header-news type="default" href="/olahraga" :active="request()->is('olahraga')" mobile="true">Olahraga</x-nav-link-header-news>
-            <x-nav-link-header-news type="default" href="/ekonomi" :active="request()->is('ekonomi')" mobile="true">Ekonomi</x-nav-link-header-news>
+            <x-nav-link-header-news type="default" href="/ekonomi" :active="request()->is('ekonomi')" mobile="true">Ekonomi</x-nav-link-header-news> -->
           </div>
         </div>
       </div>
