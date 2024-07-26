d1fc9bd206ca923cbc930d1ba5bc2024965eff55
Ikhwan kurniawan <128669107+rhenaald@users.noreply.github.com>
2024-07-26 21:34:10 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -8,15 +8,10 @@ use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\HomeController;
 
-Route::get('/home', function () {
-    return view('home', [
-        'title' => 'Home Page',
-        'posts' => Post::all()
-    ]);
-});
-
-Route::resource('panel', DashboardController::class);
+Route::get('/home', [HomeController::class, 'index']);
+Route::get('/terkini', [HomeController::class, 'terkini']);
 
 Route::get('/category/{category:slug}', function (Category $category) {
     $sub_judul = $category->name ? ' Artikel in ' . $category->name : '';
@@ -49,18 +44,16 @@ Route::get('/profile', function () {
 // another
 
 
-Route::get('/terkini', function () {
-    $posts = Post::latest()->take(5)->get();
-    return view('terkini', ['title' =>'blog terkini']);
-});
-
-
+// Route::get('/terkini', function () {
+//     $posts = Post::latest()->take(5)->get();
+//     return view('terkini', ['title' =>'blog terkini']);
+// });
 
 Route::get('/categories/{category:slug}', [CategoryController::class, 'show']);
 Route::resource('Home', HomeController::class);
 
 Route::middleware(['role:admin|editor'])->group(function () {
-    Route::view('/dashboard', 'dashboard');
+    Route::resource('panel', DashboardController::class);
     Route::resource('categories', CategoryController::class);
     Route::resource('users', UserController::class);
     Route::resource('posts', PostController::class);   
