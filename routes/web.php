f155cfe2cad6602e0e42916ae3be134c4692a06d
Ikhwan kurniawan <128669107+rhenaald@users.noreply.github.com>
2024-07-26 13:29:13 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -7,17 +7,18 @@ use App\Models\Category;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\DashboardController;
 
-Route::get('/home', function () {
+Route::get('/', function () {
     return view('home', [
         'title' => 'Home Page',
         'posts' => Post::all()
     ]);
 });
 
-// Route::post('/posts/upload-image', [PostController::class, 'uploadImage'])->name('posts.uploadImage');
 
-// kategori
+Route::resource('panel', DashboardController::class);
 
 Route::get('/categories/{category:slug}', function (Category $category) {
     $sub_judul = $category->name ? ' Artikel in ' . $category->name : '';
@@ -51,60 +52,18 @@ Route::get('/profile', function () {
 // another
 
 
-// Route::get('/terkini', function () {
-//     return view('terkini', ['title' =>'blog terkini']);
-// });
-
-// Route::get('/populer', function () {
-//     return view('terpopuler', ['title' =>'blog terpopuler']);
-// });
-
-// Route::get('/top-news', function () {
-//     return view('top-news', ['title' =>'top-news Page']);
-// });
-
-// Route::get('/pilihan-editor', function () {
-//     return view('pilihan-editor', ['title' =>'pilihan-editor Page']);
-// });
-
-// Route::get('/politik', function () {
-//     return view('politik', ['title' =>'politik Page']);
-// });
-
-// Route::get('/hiburan', function () {
-//     return view('hiburan', ['title' =>'hukum Page']);
-// });
-
-// Route::get('/pendidikan', function () {
-//     return view('pendidikan', ['title' =>'pendidikan Page']);
-// });
-
-// Route::get('/olahraga', function () {
-//     return view('olahraga', ['title' =>'olahraga Page']);
-// });
-
-// Route::get('/ekonomi', function () {
-//     return view('ekonomi', ['title' =>'ekonomi Page']);
-// });
+Route::get('/terkini', function () {
+    $posts = Post::latest()->take(5)->get();
+    return view('terkini', ['title' =>'blog terkini']);
+});
 
 
-// Route::middleware([
-//     'auth:sanctum',
-//     config('jetstream.auth_session'),
-//     'verified',
-// ])->group(function () {
-//     Route::get('/dashboard', function () {
-//         return view('dashboard');
-//     })->name('dashboard');
-// });
+Route::get('/categories/{category:slug}', [CategoryController::class, 'show']);
+Route::resource('Home', HomeController::class);
 
 Route::middleware(['role:admin|editor'])->group(function () {
     Route::view('/dashboard', 'dashboard');
     Route::resource('categories', CategoryController::class);
     Route::resource('users', UserController::class);
-    Route::resource('posts', PostController::class);
+    Route::resource('posts', PostController::class)->except('show');   
 });
-
-// Route::view('/admin', 'test')->middleware('role:admin');
-// Route::view('/editor', 'test')->middleware('role:editor');
-// Route::view('/standard', 'test')->middleware('role:standard');
