bc07f7a9fdbe3c91e03b490b5360db601d70ec15
Ikhwan kurniawan <128669107+rhenaald@users.noreply.github.com>
2024-07-26 18:59:39 +0700

    Update edit.blade.php

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -14,6 +14,21 @@
     </div>
 
     <div class="mb-5">
+    <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password Baru</label>
+    <input type="password" id="password" name="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Masukkan password baru (kosongkan jika tidak ingin mengubah)">
+    </div>
+
+    <div class="mb-5">
+        <label for="password_confirmation" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Konfirmasi Password</label>
+        <input type="password" id="password_confirmation" name="password_confirmation" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Konfirmasi password baru">
+    </div>
+
+    @error('password')
+        <div class="text-red-500 mt-2">{{ $message }}</div>
+    @enderror
+
+
+    <div class="mb-5">
         <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
         <input type="email" id="email" name="email" value="{{ old('email', $user->email) }}" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
         @error('email')
@@ -21,12 +36,18 @@
         @enderror
     </div>
 
+
     <div class="mb-5">
-        <label for="profile_photo_path" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Foto Profil</label>
-        <input type="file" id="profile_photo_path" name="profile_photo_path" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-        @error('profile_photo_path')
-            <div class="text-red-500 mt-2">{{ $message }}</div>
-        @enderror
+    <label for="role" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Role</label>
+    <select id="role" name="role" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+        <option value="" disabled selected>Pilih Role</option>
+        <option value="admin">Admin</option>
+        <option value="editor">Editor</option>
+        <option value="standard">user</option>
+    </select>
+    @error('role')
+        <div class="text-red-500 mt-2">{{ $message }}</div>
+    @enderror
     </div>
 
     <a href="{{ route('categories.index') }}"  class="text-white bg-sky-600 hover:bg-sky-800 focus:ring-4 focus:outline-none focus:ring-sky-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-3 text-center dark:bg-sky-600 dark:hover:bg-sky-700 dark:focus:ring-sky-800">
