799677f4a95147d4ef8406195c194a8231cf85cb
Ikhwan kurniawan <128669107+rhenaald@users.noreply.github.com>
2024-07-26 18:59:35 +0700

    Update create.blade.php

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -38,11 +38,16 @@
     </div>
 
     <div class="mb-5">
-    <label for="image" class="block text-gray-700">Profile Photo</label>
-    <input type="file" name="image" id="image" class="w-full border border-gray-300 p-2 rounded-lg">
-        @error('image')
-            <div class="text-red-500 mt-2">{{ $message }}</div>
-        @enderror
+    <label for="role" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Role</label>
+    <select id="role" name="role" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+        <option value="" disabled selected>Pilih Role</option>
+        <option value="admin">Admin</option>
+        <option value="editor">Editor</option>
+        <option value="standard">user</option>
+    </select>
+    @error('role')
+        <div class="text-red-500 mt-2">{{ $message }}</div>
+    @enderror
     </div>
 
     <a href="{{ route('categories.index') }}"  class="text-white bg-sky-600 hover:bg-sky-800 focus:ring-4 focus:outline-none focus:ring-sky-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-3 text-center dark:bg-sky-600 dark:hover:bg-sky-700 dark:focus:ring-sky-800">
