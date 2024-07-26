59d310e3e25437cf52d1e68b470e9f491a03b43a
Ikhwan kurniawan <128669107+rhenaald@users.noreply.github.com>
2024-07-26 18:56:15 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,15 +2,18 @@
 
 namespace App\Providers;
 
+use Laravel\Fortify\Fortify;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
 use App\Models\Category;
+
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
      */
+
     public function register(): void
     {
         //
